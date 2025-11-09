202c498c1e9de5b33598a1ce3eb31a5e87424e3a
Tung Nguyen <nvntung@gmail.com>
2025-11-09 16:39:23 +0000

    Modify the client views and configurations

--- client/app.vue
+++ client/app.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h2 style="text-transform: uppercase">Quan Ly Thu Chi Xuat Nhap</h2>
   </div>
 </template>
--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,7 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import {resolve} from "path";
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
+  alias: {
+    "@": resolve(__dirname, "/")
+  },
+  css: ["~/assets/css/main.scss"],
   devServer: {
     port: 4000
   },
